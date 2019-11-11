6c8899421763eed7e1a4ce8d9ca9b6991e789c9c
Luciano Ferrari <luciano.ferrari@mercadolibre.com>
2019-11-11 10:57:08 -0300

    format methods for defer

--- deferpanic.go
+++ deferpanic.go
@@ -1,30 +1,39 @@
 package main
 
-import 
-(
-"fmt"
-"time"
+import (
+	"fmt"
+	"time"
 )
 
 func main() {
 	go Wrap(test)()
- 	time.Sleep(time.Second)
- 	fmt.Println("HELLO")
+	time.Sleep(time.Second)
+	fmt.Println("HELLO")
 }
 
-func test(){
- 	panic("PANIC")
+func test() {
+	panic("PANIC")
 }
 
-func Wrap(f func()) func(){return func(){
- 	defer func(){if r := recover(); r !=nil{
- 	fmt.Printf("RECOVERED - %v\r\n", r)}}()
- 	f()}
+func Wrap(f func()) func() {
+	return func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("RECOVERED - %v\r\n", r)
+			}
+		}()
+		f()
+	}
 }
 
-func WrapWithSignal(f func(chan bool), signal chan bool) func(){return func(){
-	 defer func(){if r := recover(); r !=nil{
-	 fmt.Printf("RECOVERED - %v\r\n", r)
-	 signal <-false}}()
-	 f(signal)}
- }
+func WrapWithSignal(f func(chan bool), signal chan bool) func() {
+	return func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("RECOVERED - %v\r\n", r)
+				signal <- false
+			}
+		}()
+		f(signal)
+	}
+}
