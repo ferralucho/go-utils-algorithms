21a867ee6e9db3016aed3fc2f517be50bbf0585c
Luciano Ferrari <luciano.ferrari@mercadolibre.com>
2020-12-13 23:11:18 -0300

    waitgroup logic

---
+++ concurrency/waitgroup/waitgroup.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func doSomething(wg *sync.WaitGroup) {
+	// do something here
+	fmt.Println("Done")
+	defer wg.Done()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+	go doSomething(&wg)
+
+	// program will wait until doSomething & doSomethingElseSync is complete
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			default:
+			}
+		}
+	}()
+	done <- true
+	fmt.Println("Done channel")
+}
