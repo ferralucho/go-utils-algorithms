ac285ec4ee2afd849e1f833991fa638abe82cf84
Luciano Ferrari <luciano.ferrari@mercadolibre.com>
2020-12-20 17:24:52 -0300

    url shortener

--- tdd/url_shortener.go
+++ tdd/url_shortener.go
@@ -1,1 +1,52 @@
 package tdd
+
+import (
+	"errors"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+// Step 2: Write implementation code to get the tests to pass
+type URLShortener struct {
+	store map[string]string
+	i     int
+}
+
+/*
+// Step 4: Write the implementaton to get the tests to pass
+func (u *URLShortener) Shorten(in string) (string, error) {
+	if !strings.Contains(in, ".com") {
+		return "", errors.New("invalid")
+	}
+
+	if !strings.Contains(in, "https://") {
+		in = fmt.Sprintf("https://%s", in)
+	}
+
+	u.i++
+	val := strconv.Itoa(u.i)
+	u.store[val] = in
+
+	return val, nil
+}
+*/
+
+// Step 5: Refactor the code and make sure that the tests still pass.
+//         Use code coverage tools to ensure that no new behaviour was added.
+func (u *URLShortener) Shorten(in string) (string, error) {
+	_, err := url.ParseRequestURI(in)
+	if err != nil {
+		return "", errors.New("invalid")
+	}
+
+	if !strings.Contains(in, ".") {
+		return "", errors.New("invalid")
+	}
+
+	u.i++
+	val := strconv.Itoa(u.i)
+	u.store[val] = in
+
+	return val, nil
+}
--- tdd/url_shortener_test.go
+++ tdd/url_shortener_test.go
@@ -3,8 +3,11 @@ package tdd
 import (
 	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+/*
 // Step 1: Write a failing test
 func TestURLShortner(t *testing.T) {
 	var tests = []struct {
@@ -18,6 +21,27 @@ func TestURLShortner(t *testing.T) {
 	for _, test := range tests {
 		t.Run("Shorten"+test.name, func(t *testing.T) {
 			s := &URLShortner{i: 0, store: make(map[string]string)}
+		})
+	}
+}
+*/
+
+// Step 3: Write test code to test the behaviour
+func TestURLShortner(t *testing.T) {
+	var tests = []struct {
+		name, in, expected string
+		err                error
+	}{
+		{"NoScheme", "https://google.com", "1", nil},
+		{"NoScheme", "google", "", errors.New("invalid")},
+	}
+
+	for _, test := range tests {
+		t.Run("Shorten"+test.name, func(t *testing.T) {
+			s := &URLShortener{i: 0, store: make(map[string]string)}
+			resp, err := s.Shorten(test.in)
+			assert.Equal(t, test.err, err)
+			assert.Equal(t, test.expected, resp)
 		})
 	}
 }
