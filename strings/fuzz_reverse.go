adf4093a3c05545430dd2e408ca77a3bf14205fc
ferraluchomoen <luciano.ferrari@fbgpg.com>
2022-07-27 00:03:09 -0300

    reverse examples

--- strings/fuzz_reverse.go
+++ strings/fuzz_reverse.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev := Reverse(input)
+	doubleRev := Reverse(rev)
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("reversed: %q\n", rev)
+	fmt.Printf("reversed again: %q\n", doubleRev)
+}
+
 func Reverse(s string) string {
 	b := []byte(s)
 	for i, j := 0, len(b)-1; i < len(b)/2; i, j = i+1, j-1 {
