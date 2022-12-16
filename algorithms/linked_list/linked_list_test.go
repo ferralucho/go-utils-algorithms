372cfcb693848d3b3815bf002e9d15e2e0de6113
Luciano Ferrari <lucianodarioferrari@gmail.com>
2022-12-16 17:31:13 -0300

    test find by val

--- algorithms/linked_list/linked_list_test.go
+++ algorithms/linked_list/linked_list_test.go
@@ -22,29 +22,58 @@ func createSinglyLinkedList(nums []int) *LinkedList {
 	return list
 }
 
-func TestRemoveElements(t *testing.T) {
+func TestInsertNodes(t *testing.T) {
+	list := createSinglyLinkedList([]int{1, 2, 6, 3, 4, 5})
+	val := 8
+	expected := createSinglyLinkedList([]int{1, 2, 3, 4, 5, 8})
+
+	list.Insert(val)
+	if list.length == expected.length {
+		t.Errorf("expected %v, got %v", expected.length, list.length)
+	}
+}
+
+func TestRemoveNodes(t *testing.T) {
 	list := createSinglyLinkedList([]int{1, 2, 6, 3, 4, 5})
 	val := 5
 	expected := createSinglyLinkedList([]int{1, 2, 3, 4})
 
 	if res := list.Remove(val); res && list.length == expected.length {
-		t.Errorf("expected %v, got %v", expected, true)
+		t.Errorf("expected %v, got %v", true, res)
 	}
 }
 
-func TestRemoveMultipleElements(t *testing.T) {
+func TestRemoveMultipleNodes(t *testing.T) {
 	list := createSinglyLinkedList([]int{1, 2, 6, 3, 4, 5})
 	firstVal := 5
 	expected := createSinglyLinkedList([]int{1, 2, 3, 4})
 
 	if res := list.Remove(firstVal); res && list.length == expected.length {
-		t.Errorf("expected %v, got %v", expected, true)
+		t.Errorf("expected %v, got %v", true, res)
 	}
 
 	secondVal := 2
 	secondExpected := createSinglyLinkedList([]int{1, 3, 4})
 
 	if res := list.Remove(secondVal); res && list.length == secondExpected.length {
-		t.Errorf("expected %v, got %v", expected, true)
+		t.Errorf("expected %v, got %v", true, res)
+	}
+}
+
+func TestFindByExistentValue(t *testing.T) {
+	list := createSinglyLinkedList([]int{1, 2, 6, 3, 4, 5})
+	val := 5
+
+	if res := list.Find(val); !res {
+		t.Errorf("expected %v, got %v", true, res)
+	}
+}
+
+func TestFindByNonExistentValue(t *testing.T) {
+	list := createSinglyLinkedList([]int{1, 2, 6, 3, 4, 5})
+	val := 11
+
+	if res := list.Find(val); res {
+		t.Errorf("expected %v, got %v", true, res)
 	}
 }
